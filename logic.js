30dd62bdbc0a8d09f1f411edf8ab9a1fbd115185
Marius Alexandru <amarius2000@gmail.com>
2021-07-28 17:13:07 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -43,98 +43,39 @@ var section1 = new Vue({
 
 
 
-var info = document.getElementById('info');
-window.onYouTubeIframeAPIReady = function()  {
-  var player = new YT.Player('player', {
-      videoId: 'b80Jw8MuZxo', // this is the id of the video at youtube (the stuff after "?v=")
-      loop: true,
-      playerVars: { 'autoplay': 0, 'controls': 1, 'playlist':['aF5nhMIyeqI', 'y7kvGqiJC4g'], },
-      events: {
-          onReady: function (e) {
-                info.innerHTML = 'video is loaded';
-                
-          },
-          
-          onStateChange: function (event) {
-              if (event.data === 1) {
-                  info.innerHTML = 'video started playing';
-                  document.querySelector('[play]').textContent = 'pause';
-                  $("[play]").addClass("activated");
-                  $("[thumb]").removeClass("bwpaused");
-                  $(".playreactbkg").removeClass("bwpaused");
-                  
-              }
-              else 
-              {
-                info.innerHTML = 'video paused';
-                document.querySelector('[play]').textContent = 'play_arrow';
-                $("[play]").removeClass("activated");
-                $("[thumb]").addClass("bwpaused");
-                $(".playreactbkg").addClass("bwpaused");
-              }
-              if (event.data == YT.PlayerState.PLAYING) {
-                var url = event.target.getVideoUrl();
-                // "http://www.youtube.com/watch?v=gzDS-Kfd5XQ&feature=..."
-                var match = url.match(/[?&]v=([^&]+)/);
-                // ["?v=gzDS-Kfd5XQ", "gzDS-Kfd5XQ"]
-                videoId = match[1];
 
-                var thumby = "https://i1.ytimg.com/vi/" + videoId + "/sddefault.jpg";
-             
 
-                $.getJSON('https://noembed.com/embed',
-                    {format: 'json', url: url}, function (data) {
-                    document.querySelector('.title').textContent = data.title;
-                    document.querySelector('.titleside').textContent = data.title;
-                });
+window.videoApiLoaded = [];
+window.videoApiLoaded.youtube = false;
 
-                    window.setInterval(function(){
-                        var s = Math.trunc(player.getDuration() - player.getCurrentTime());
-                        var minutes = Math.floor(s / 60);
-                        var seconds = s - minutes * 60;
-                        document.querySelector('[time]').textContent = minutes + ':' + seconds;
-                      }, 1000);
-                    
-             
+function loadYoutubeVideo(videoId) {
 
-                
-            }
-             
+    window.onYouTubeIframeAPIReady = function() { document.dispatchEvent(new CustomEvent('onYouTubeIframeAPIReady', {})) };
+
+    if(window.videoApiLoaded.youtube == false) {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        window.videoApiLoaded.youtube = true;
+    }
+
+    var player;
+
+    document.addEventListener('onYouTubeIframeAPIReady', function (e) {
+        player = new YT.Player('player', {
+          height: '400',
+          width: '600',
+          videoId: videoId,
+          events: {
+            'onStateChange': onYtStateChange
           }
-      }
-  });
+        });
 
+    }, false);
 
-
-  function play() {
-        player.playVideo() 
-        $(this).one("click", pause);
-     
-    }
-    function pause() {
-        player.pauseVideo() 
-        $(this).one("click", play);
-
-    }
-    $("[play]").one("click", play);
-
-    $("[skip-next]").on("mousedown", function() {
-        player.nextVideo();
-    });
-    $("[skip-prev]").on("mousedown", function() {
-        player.previousVideo();
-    });
-
-
-   
-    $("[blues1]").on("mousedown", function() {
-        player.loadVideoById({'videoId': '1eNSWZ4x2ZU'});
-    });
-   
-};
-
+}
 
 $("[tame]").on("mousedown", function() {
     player.loadPlaylist({list: "PLGFMsDB0B5xyqR0LgYHa79ZsWDxBSE_Kq", index: 0, startSeconds: 0,suggestedQuality: "small"});
 });
-
