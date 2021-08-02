757fe24f2e1fd1d544730ff2a45e7a7882d57a43
Marius Alexandru <amariuscontact@gmail.com>
2021-08-02 12:40:39 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -59,7 +59,7 @@ var app = new Vue({
     },
     methods: {
       playlistPlay: function (event) {
-        player.loadPlaylist({list:+'\''+item.list+'\''});
+        player.loadPlaylist({list:+'\''+this.list+'\''});
       }
     }
   });
