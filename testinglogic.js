6d9e1bb997a97f1b04ddfb95dd14a2604a964d2c
Marius Alexandru <amariuscontact@gmail.com>
2021-08-03 17:48:45 +0300

    Update testinglogic.js

--- testinglogic.js
+++ testinglogic.js
@@ -71,12 +71,15 @@ $('a[action="#closedialog"]').click(function() {
 })
 
 
+      // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -130,3 +133,7 @@ $('a[action="#closedialog"]').click(function() {
     $("[skip-prev]").on("mousedown", function() {
         player.previousVideo();
     });
+    //$(".activatrix").on("click", function() {
+    //  $(".activatrix").click();
+    //  alert("clicked");
+    //});
